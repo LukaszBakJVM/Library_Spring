8bc91fd7260b49757098af5774362bba3566c435
Łukasz Bąk <bbzzyyczczeek@interia.pl>
2022-12-26 14:29:06 +0100

    loop

--- src/main/java/com/example/library_spring_h2/Loop.java
+++ src/main/java/com/example/library_spring_h2/Loop.java
@@ -5,17 +5,22 @@ import org.springframework.stereotype.Service;
 import java.util.Scanner;
 @Service
 public class Loop {
-    public Loop(Scanner scanner) {
-        this.scanner = scanner;
-    }
-
     private final   Scanner scanner;
+    private final LibraryService libraryService;
+
+    public Loop(Scanner scanner, LibraryService libraryService) {
+        this.scanner = scanner;
+        this.libraryService = libraryService;
+    }
 
     public void loopControl(){
         Options options;
         do {
             printOptions();
             options=Options.createOptions(scanner.nextInt());
+            switch (options){
+                case ADD -> libraryService.addNewBook(addBook());
+            }
 
         }while (options!=Options.EXIT);
 
@@ -29,5 +34,18 @@ public class Loop {
             System.out.println(option);
 
         }
+    }
+    private LibraryDto addBook(){
+        System.out.println("Podaj nazwe ksiazki");
+        String bookName=scanner.next();
+        System.out.println("Podaj Autora");
+        String author=scanner.next();
+        System.out.println("Podaj ilosc stron");
+        int pagesNumber=scanner.nextInt();
+        System.out.println("Podaj kategorie");
+        String category =scanner.next();
+        System.out.println("Podaj numer ISBN");
+        int isbn=scanner.nextInt();
+        return new LibraryDto(bookName,author,pagesNumber,category,isbn);
     }
 }
