dc4da9f3952ed36d0f4e656a1225f53e93d6f68b
Łukasz Bąk <bbzzyyczczeek@interia.pl>
2022-12-26 14:30:25 +0100

    loop

--- src/main/java/com/example/library_spring_h2/Options.java
+++ src/main/java/com/example/library_spring_h2/Options.java
@@ -1,7 +1,7 @@
 package com.example.library_spring_h2;
 
 public enum Options {
-    EXIT(0,"Wyjscie"),ADD(1,"Dodaj ksiazke"),RENT(2,"Wyporzycz ksiazke"),DELETE_BOOK(3,"Usun ksiazke");
+    EXIT(0,"Wyjscie"),ADD(1,"Dodaj ksiazke"),RENT(2,"Wypozycz ksiazke"),DELETE_BOOK(3,"Usun ksiazke");
     final private int option;
     final private String descryption;
 
